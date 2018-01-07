c79378f25046f8d5eb006f1525a660d69ee7fad7
Steven Zamorano <stevenzamorano.code@gmail.com>
2018-01-07 14:53:26 -0500

    Initial commit

---
+++ Controllers/EmailController.cs
@@ -0,0 +1,45 @@
+﻿using Newtonsoft.Json;
+using System;
+using System.Collections.Generic;
+using System.Configuration;
+using System.Linq;
+using System.Net.Mail;
+using System.Threading.Tasks;
+using System.Web;
+using System.Web.Mvc;
+using sZamorano_Portfolio.Models;
+
+namespace sZamorano_Portfolio.Controllers
+{
+    public class EmailController : Controller
+    {
+        // GET: Email
+        public async Task EmailGo(string emailName, string emailAddress, string emailBody)
+        {
+
+            try
+            {
+                var body = "<p>Email From: <bold>{0}</bold>({1})</p><p>Message:</p><p>{2}</p>";
+                var from = "MyPortfolio<Example@email.com>";
+                //model.Body = "This is a message from your portfolio site. The name and the email of the contacting person is above.";
+
+
+                var email = new MailMessage(from,
+                        ConfigurationManager.AppSettings["emailto"])
+                {
+                    Subject = "Portfolio Contact Email",
+                    Body = string.Format(body, emailName, emailAddress,
+                            emailBody),
+                    IsBodyHtml = true
+                };
+                var svc = new EmailHelper();
+                await svc.SendAsync(email);
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine(ex.Message);
+                await Task.FromResult(0);
+            }
+        }
+    }
+}
